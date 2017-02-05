b5249fc5641c3bf2c9d0c8077a4bf2d3fd071503
Bhuvanesh Verma <workoholic24x7@gmail.com>
2017-02-05 17:48:14 +0530

    Done1

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-res.sendFile('Under Construction!');
+res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
-res.sendFile('Under Construction!!');
+res.send('Under Construction!!');
 });
 
 app.get('/article3',function(req,res){
-res.sendFile('Under Construction!!!');
+res.send('Under Construction!!!');
 });
 
 app.get('/ui/style.css', function (req, res) {
