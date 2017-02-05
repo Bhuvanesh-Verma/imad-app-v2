0da0dd06d2ef31add2d07ddbb035750033ce30e4
Bhuvanesh Verma <workoholic24x7@gmail.com>
2017-02-05 17:18:51 +0530

    Done

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+res.sendFile('Under Construction!');
+});
+
+app.get('/article-two',function(req,res){
+res.sendFile('Under Construction!!');
+});
+
+app.get('/article3',function(req,res){
+res.sendFile('Under Construction!!!');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
