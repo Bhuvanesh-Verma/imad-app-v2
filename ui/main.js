d2808c22143d59e10f2505bcbc22b1a0744f1000
Bhuvanesh-Verma <workoholic24x7@gmail.com>
2017-02-12 18:49:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,10 +53,43 @@ submit.onclick=function(){
         
     };
     var nameInput=document.getElementById('name');
-var name=nameInput.value;
+    var name=nameInput.value;
  
    req.open('GET','http://bhuvanesh-verma.imad.hasura-app.io/getname?name='+name,true);
    req.send(null);
     
  
+};
+
+var send=document.getElementById('Send');
+send.onClick=function(){
+    
+    
+     var req = new XMLHttpRequest();
+    
+    req.onreadystatechange=function(){
+        
+        if(req.readyState===XMLHttpRequest.DONE)
+        {
+            if(req.status===200)
+            {
+               var names=req.responseText;
+               names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++)
+                {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ol=document.getElementById('tu');
+                ol.innerHTML=list;
+            }
+        }
+        
+    };
+    var commentInput=document.getElementById('comments');
+var comm=commentInput.value;
+ 
+   req.open('GET','http://bhuvanesh-verma.imad.hasura-app.io/getcomm?name='+commentInput,true);
+   req.send(null);
+    
 };
